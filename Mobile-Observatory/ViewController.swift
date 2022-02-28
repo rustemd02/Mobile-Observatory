e64533175160efb8f2e42b7ab47f3f1336d1f019
rustemd02 <rustemd02@mail.ru>
2022-02-28 20:02:55 +0300

    Initial Commit

---
+++ Mobile-Observatory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobile-Observatory
+//
+//  Created by Рустем on 28.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
