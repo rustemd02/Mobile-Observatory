67b31871c0eed333a0f51983850b90dadf7dfb4b
AndrewOch <38253789+AndrewOch@users.noreply.github.com>
2022-04-07 11:23:56 +0300

    Fixes

---
+++ Mobile-Observatory/Modules/SavedPostsModule/SavedPostsInteractor.swift
@@ -0,0 +1,17 @@
+//
+//  SavedPostsInteractor.swift
+//  Mobile-Observatory
+//
+//  Created by andrewoch on 07.04.2022.
+//
+
+import Foundation
+
+class SavedPostsInteractor {
+    
+    var coreDataService: CoreDataService
+    
+    init(){
+        coreDataService = CoreDataService.shared
+    }
+}
