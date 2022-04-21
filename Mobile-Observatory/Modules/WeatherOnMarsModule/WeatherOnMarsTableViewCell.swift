1bdac7bb2477ec7bc5e49421939fd0b30423bc97
rustemd02 <rustemd02@mail.ru>
2022-04-21 16:51:51 +0300

    WeatherOnMars files

---
+++ Mobile-Observatory/Modules/WeatherOnMarsModule/WeatherOnMarsTableViewCell.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class WeatherOnMarsTableViewCell: UITableViewCell {
+    let api = NetworkService.shared
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var solLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    func configure() {
+        let _ = api.getWeatherData(sol: "") { [weak self] result in
+            switch result {
+            case .success(let weatherInfo):
+                self?.temperatureLabel.text = weatherInfo.minTemp.description + "°C"
+                self?.solLabel.text = weatherInfo.sol.description + " сол"
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
